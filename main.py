743e8e7e4aa5016b1ecf8265642a3d5611d40fa5
Matin <MatiinBaloochestani@Gmail.com>
2021-10-12 13:01:02 +0330

    Set text for tweet with no media

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import logging
-import config
-from aiogram import Bot, Dispatcher, executor, types
-from downlaoder import download_tweet
 
+from aiogram import Bot, Dispatcher, executor, types
+from aiogram.types import ParseMode
+from aiogram.utils.emoji import emojize
+
+import config
+from downlaoder import download_tweet
 
 # Configure logging
 logging.basicConfig(level=logging.INFO)
@@ -22,12 +25,43 @@ async def send_welcome(message: types.Message):
 
 @dp.message_handler()
 async def send_download_link(message: types.Message):
+    """
+    This handler will be called when user sends any text message
+    """
     message_text = message.text
     download_response = download_tweet(message_text)
-    if download_response == "Media Not Found":
-        await message.reply("Media Not Found")
-    elif download_response == "Not Found":
+    if download_response == "Not Found":
         await message.reply("Tweet Not Found")
+    else:
+        tweet_text = download_response['text']
+        tweet_author_username = download_response['tweet_author_username']
+        tweet_author_name = download_response['tweet_author_name']
+        tweet_date = download_response['tweet_date']
+        link = download_response['link']
+        media_status = download_response['media']
+
+        if media_status is True:
+            pass
+        else:
+            await message.reply(
+                # https://www.webfx.com/tools/emoji-cheat-sheet/#
+                emojize(f'{tweet_text}\n\n:link: [{tweet_author_name} '\
+                        f'(@{tweet_author_username})](https://twitter.com/{tweet_author_username})'\
+                        '\n\n:robot: @TwitterMediaDownloaderBot'),
+                parse_mode=ParseMode.MARKDOWN,
+                disable_web_page_preview=True
+            )
+
+"""
+    if len(download_response) != 1:
+        # Create media group
+        media = types.MediaGroup()
+
+        for item in download_response:
+            media.attach_photo(item['url'])
+        await message.reply_media_group(
+            media=media
+        )
     else:
         for item in download_response:
             if item['type'] == 'video':
@@ -36,7 +70,7 @@ async def send_download_link(message: types.Message):
                 await message.reply_photo(item['url'])
             elif item['type'] == 'animated_gif':
                 await message.reply_animation(item['url'])
-
+"""
 
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True)
